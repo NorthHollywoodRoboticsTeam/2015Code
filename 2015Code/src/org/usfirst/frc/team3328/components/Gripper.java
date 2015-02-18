f0ba3e86828f65d93ca0f61a59aace8bd71f65fc
Henry Birge-Lee <henrybirgelee@gmail.com>
2015-02-17 19:19:04 -0800

    Started grabber

--- 2015Code/src/org/usfirst/frc/team3328/components/Gripper.java
+++ 2015Code/src/org/usfirst/frc/team3328/components/Gripper.java
@@ -1,15 +1,25 @@
 package org.usfirst.frc.team3328.components;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Talon;
 
 public class Gripper {
 	
 	Talon gripperTalon = new Talon(5);
 	
+	
+	Joystick controlStick;
+	
+	public Gripper(Joystick controlStick) {
+		this.controlStick = controlStick;
+	}
+	
+	
 	public void init() {
 	}
 	
 	public void teleopPeriodic() {
+		
 	}
 	
 	public void autonomousPeriodic() {
